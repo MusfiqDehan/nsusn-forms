f3e8bae4804297d6498aea57b4204d638c9018fc
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2024-01-24 02:31:27 +0600

    Add ContactForm component with EmailJS

---
+++ frontend/src/components/ContactForm.jsx
@@ -0,0 +1,42 @@
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+const ContactForm = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "nsusn-contact-mrd",
+        "nsusn-contact-mrd",
+        form.current,
+        "mH6d_RzQNvNI8N3_e",
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        },
+      );
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Subject</label>
+      <input type="text" name="subject" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+
+export default ContactForm;
