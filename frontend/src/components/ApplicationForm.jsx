9ac5222908eaaa81aeed0f71c9c42dce4e1f87a6
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2024-01-26 01:52:52 +0600

    Update Application Form style

--- frontend/src/components/ApplicationForm.jsx
+++ frontend/src/components/ApplicationForm.jsx
@@ -52,198 +52,251 @@ const ApplicationForm = () => {
   };
 
   return (
-    <>
-      <h1 className="text-3xl font-bold">Application Form</h1>
-      <form onSubmit={handleSubmit}>
-        <label className="block p-5">
-          Name:
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </label>
-        <label className="block p-5">
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label className="block p-5">
-          Contact No:
-          <input
-            type="text"
-            value={contactNo}
-            onChange={(e) => setContactNo(e.target.value)}
-            required
-          />
-        </label>
-        <label className="block p-5">
-          LinkedIn Link:
-          <input
-            type="text"
-            value={linkedinLink}
-            onChange={(e) => setLinkedinLink(e.target.value)}
-            required
-          />
-        </label>
-        <label className="block p-5">
-          Idea Name:
-          <input
-            type="text"
-            value={ideaName}
-            onChange={(e) => setIdeaName(e.target.value)}
-            required
-          />
-        </label>
-        <label className="block p-5">
-          Startup Site Link:
-          <input
-            type="text"
-            value={startupSiteLink}
-            onChange={(e) => setStartupSiteLink(e.target.value)}
-            required
-          />
-        </label>
-        <label className="block p-5">
-          FB Page Link:
-          <input
-            type="text"
-            value={fbPageLink}
-            onChange={(e) => setFBPageLink(e.target.value)}
-            required
-          />
-        </label>
-        <label className="block p-5">
-          Stream Category:
-          <div>
-            <label className="block p-5">
+    <div id="apply-now" className="pt-28">
+      <h1 className="text-center text-3xl font-bold">Application Form</h1>
+      <div className="flex items-center justify-center p-12">
+        <div className="mx-auto w-full max-w-[550px]">
+          <form onSubmit={handleSubmit}>
+            <div className="my-3">
+              <label className="mb-3 block text-base font-medium text-inherit">
+                Name
+              </label>
               <input
-                type="radio"
-                value="Impact"
-                checked={streamCategory === "Impact"}
-                onChange={(e) => setStreamCategory(e.target.value)}
-              />
-              Impact
-            </label>
-            <label className="block p-5">
-              <input
-                type="radio"
-                value="Women-led"
-                checked={streamCategory === "Women-led"}
-                onChange={(e) => setStreamCategory(e.target.value)}
-              />
-              Women-led
-            </label>
-            <label className="block p-5">
-              <input
-                type="radio"
-                value="Grassroots"
-                checked={streamCategory === "Grassroots"}
-                onChange={(e) => setStreamCategory(e.target.value)}
-              />
-              Grassroots
-            </label>
-          </div>
-        </label>
-        <label className="block p-5">
-          Domain Type:
-          <input
-            type="text"
-            value={domainType}
-            onChange={(e) => setDomainType(e.target.value)}
-            required
-          />
-        </label>
-        <label className="block p-5">
-          Problem:
-          <input
-            type="text"
-            value={problem}
-            onChange={(e) => setProblem(e.target.value)}
-            required
-          />
-        </label>
-        <label className="block p-5">
-          Solution:
-          <input
-            type="text"
-            value={solution}
-            onChange={(e) => setSolution(e.target.value)}
-            required
-          />
-        </label>
-        <label className="block p-5">
-          Cofounder Details:
-          <input
-            type="text"
-            value={cofounderDetails}
-            onChange={(e) => setCofounderDetails(e.target.value)}
-            required
-          />
-        </label>
-        <label className="block p-5">
-          Has MVP:
-          <div>
-            <label className="block p-5">
-              <input
-                type="radio"
-                value="Yes"
-                checked={hasMVP === "Yes"}
-                onChange={(e) => setHasMVP(e.target.value)}
+                className="w-full rounded-md border border-[#e0e0e0] bg-white px-6 py-3 text-base font-medium text-[#6B7280] outline-none focus:border-[#59358a] focus:shadow-md"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 required
               />
-              Yes
-            </label>
-            <label className="block p-5">
+            </div>
+            <div className="my-3">
+              <label className="mb-3 block text-base font-medium text-inherit">
+                Email
+              </label>
               <input
-                type="radio"
-                value="No"
-                checked={hasMVP === "No"}
-                onChange={(e) => setHasMVP(e.target.value)}
+                className="w-full rounded-md border border-[#e0e0e0] bg-white px-6 py-3 text-base font-medium text-[#6B7280] outline-none focus:border-[#59358a] focus:shadow-md"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required
               />
-              No
-            </label>
-          </div>
-        </label>
-        <label className="block p-5">
-          Pitch Deck File Link:
-          <input
-            type="text"
-            onChange={(e) => setPitchDeckFile(e.target.value)}
-            required
-          />
-        </label>
-        <label className="block p-5">
-          Product Demo Link:
-          <input
-            type="text"
-            value={productDemoLink}
-            onChange={(e) => setProductDemoLink(e.target.value)}
-            required
-          />
-        </label>
-        <label className="block p-5">
-          Expected Support:
-          <input
-            type="text"
-            value={expectedSupport}
-            onChange={(e) => setExpectedSupport(e.target.value)}
-            required
-          />
-        </label>
-        <input
-          className="m-3 rounded-md bg-red-400 p-3"
-          type="submit"
-          value="Submit"
-        />
-      </form>
-    </>
+            </div>
+            <div className="my-3">
+              <label className="mb-3 block text-base font-medium text-inherit">
+                Contact No
+              </label>
+              <input
+                className="w-full rounded-md border border-[#e0e0e0] bg-white px-6 py-3 text-base font-medium text-[#6B7280] outline-none focus:border-[#59358a] focus:shadow-md"
+                type="text"
+                value={contactNo}
+                onChange={(e) => setContactNo(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="my-3">
+              <label className="mb-3 block text-base font-medium text-inherit">
+                LinkedIn Link
+              </label>
+              <input
+                className="w-full rounded-md border border-[#e0e0e0] bg-white px-6 py-3 text-base font-medium text-[#6B7280] outline-none focus:border-[#59358a] focus:shadow-md"
+                type="text"
+                value={linkedinLink}
+                onChange={(e) => setLinkedinLink(e.target.value)}
+                required
+              />
+            </div>
+            <div className="my-3">
+              <label className="mb-3 block text-base font-medium text-inherit">
+                Idea Name
+              </label>
+              <input
+                className="w-full rounded-md border border-[#e0e0e0] bg-white px-6 py-3 text-base font-medium text-[#6B7280] outline-none focus:border-[#59358a] focus:shadow-md"
+                type="text"
+                value={ideaName}
+                onChange={(e) => setIdeaName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="my-3">
+              <label className="mb-3 block text-base font-medium text-inherit">
+                Startup Site Link
+              </label>
+              <input
+                className="w-full rounded-md border border-[#e0e0e0] bg-white px-6 py-3 text-base font-medium text-[#6B7280] outline-none focus:border-[#59358a] focus:shadow-md"
+                type="text"
+                value={startupSiteLink}
+                onChange={(e) => setStartupSiteLink(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="my-3">
+              <label className="mb-3 block text-base font-medium text-inherit">
+                FB Page Link
+              </label>
+              <input
+                className="w-full rounded-md border border-[#e0e0e0] bg-white px-6 py-3 text-base font-medium text-[#6B7280] outline-none focus:border-[#59358a] focus:shadow-md"
+                type="text"
+                value={fbPageLink}
+                onChange={(e) => setFBPageLink(e.target.value)}
+                required
+              />
+            </div>
+            <div className="my-3">
+              <label className="mb-3 block text-base font-medium text-inherit">
+                Stream Category
+              </label>
+              <div className="flex items-center space-x-6">
+                <label className="flex items-center">
+                  <input
+                    className="mr-2 h-5 w-5"
+                    type="radio"
+                    value="Impact"
+                    checked={streamCategory === "Impact"}
+                    onChange={(e) => setStreamCategory(e.target.value)}
+                  />
+                  Impact
+                </label>
+                <label className="flex items-center">
+                  <input
+                    className="mr-2 h-5 w-5"
+                    type="radio"
+                    value="Women-led"
+                    checked={streamCategory === "Women-led"}
+                    onChange={(e) => setStreamCategory(e.target.value)}
+                  />
+                  Women-led
+                </label>
+                <label className="flex items-center">
+                  <input
+                    className="mr-2 h-5 w-5"
+                    type="radio"
+                    value="Grassroots"
+                    checked={streamCategory === "Grassroots"}
+                    onChange={(e) => setStreamCategory(e.target.value)}
+                  />
+                  Grassroots
+                </label>
+              </div>
+            </div>
+            <div className="my-3">
+              <label className="mb-3 block text-base font-medium text-inherit">
+                Domain Type
+              </label>
+              <input
+                className="w-full rounded-md border border-[#e0e0e0] bg-white px-6 py-3 text-base font-medium text-[#6B7280] outline-none focus:border-[#59358a] focus:shadow-md"
+                type="text"
+                value={domainType}
+                onChange={(e) => setDomainType(e.target.value)}
+                required
+              />
+            </div>
+            <div className="my-3">
+              <label className="mb-3 block text-base font-medium text-inherit">
+                Problem Statement
+              </label>
+              <input
+                className="w-full rounded-md border border-[#e0e0e0] bg-white px-6 py-3 text-base font-medium text-[#6B7280] outline-none focus:border-[#59358a] focus:shadow-md"
+                type="text"
+                value={problem}
+                onChange={(e) => setProblem(e.target.value)}
+                required
+              />
+            </div>
+            <div className="my-3">
+              <label className="mb-3 block text-base font-medium text-inherit">
+                Your Solution
+              </label>
+              <input
+                className="w-full rounded-md border border-[#e0e0e0] bg-white px-6 py-3 text-base font-medium text-[#6B7280] outline-none focus:border-[#59358a] focus:shadow-md"
+                type="text"
+                value={solution}
+                onChange={(e) => setSolution(e.target.value)}
+                required
+              />
+            </div>
+            <div className="my-3">
+              <label className="mb-3 block text-base font-medium  text-inherit">
+                Cofounder Details
+              </label>
+              <input
+                className="w-full rounded-md border border-[#e0e0e0] bg-white px-6 py-3 text-base font-medium text-[#6B7280] outline-none focus:border-[#59358a] focus:shadow-md"
+                type="text"
+                value={cofounderDetails}
+                onChange={(e) => setCofounderDetails(e.target.value)}
+                required
+              />
+            </div>
+            <div className="my-3">
+              <label className="mb-3 block text-base font-medium text-inherit">
+                Do you have any MVP?
+              </label>
+              <div className="flex items-center space-x-6">
+                <label className="flex items-center">
+                  <input
+                    className="mr-2 h-5 w-5"
+                    type="radio"
+                    value="Yes"
+                    checked={hasMVP === "Yes"}
+                    onChange={(e) => setHasMVP(e.target.value)}
+                    required
+                  />
+                  Yes
+                </label>
+                <label className="flex items-center">
+                  <input
+                    className="mr-2 h-5 w-5"
+                    type="radio"
+                    value="No"
+                    checked={hasMVP === "No"}
+                    onChange={(e) => setHasMVP(e.target.value)}
+                    required
+                  />
+                  No
+                </label>
+              </div>
+            </div>
+            <div className="my-3">
+              <label className="mb-3 block text-base font-medium text-inherit">
+                Pitch Deck File Link
+              </label>
+              <input
+                className="w-full rounded-md border border-[#e0e0e0] bg-white px-6 py-3 text-base font-medium text-[#6B7280] outline-none focus:border-[#59358a] focus:shadow-md"
+                type="text"
+                onChange={(e) => setPitchDeckFile(e.target.value)}
+                required
+              />
+            </div>
+            <div className="my-3">
+              <label className="mb-3 block text-base font-medium text-inherit">
+                Product Demo Link
+              </label>
+              <input
+                className="w-full rounded-md border border-[#e0e0e0] bg-white px-6 py-3 text-base font-medium text-[#6B7280] outline-none focus:border-[#59358a] focus:shadow-md"
+                type="text"
+                value={productDemoLink}
+                onChange={(e) => setProductDemoLink(e.target.value)}
+                required
+              />
+            </div>
+            <div className="my-3">
+              <label className="mb-3 block text-base font-medium text-inherit">
+                Expected Support
+              </label>
+              <input
+                className="w-full rounded-md border border-[#e0e0e0] bg-white px-6 py-3 text-base font-medium text-[#6B7280] outline-none focus:border-[#59358a] focus:shadow-md"
+                type="text"
+                value={expectedSupport}
+                onChange={(e) => setExpectedSupport(e.target.value)}
+                required
+              />
+            </div>
+            <input className="form__button" type="submit" value="Submit" />
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
