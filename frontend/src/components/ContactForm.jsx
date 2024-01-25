7418c05a3d22bd3decb451f7f7043ea04806c067
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2024-01-25 14:53:34 +0600

    Refactor ContactForm component and add animation

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -1,7 +1,18 @@
 import { useRef } from "react";
 import emailjs from "@emailjs/browser";
+import Lottie from "react-lottie";
+import ContactAnimation from "../lotties/ContactAnimation.json";
 
 const ContactForm = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: ContactAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -25,51 +36,64 @@ const ContactForm = () => {
   };
 
   return (
-    <>
-      <h1 className="text-3xl font-bold">Contact Form</h1>
-      <form ref={form} onSubmit={sendEmail}>
-        <label className="block p-5">
-          Name
-          <input
-            type="text"
-            name="user_name"
-            placeholder="Type here"
-            className="input input-bordered w-full max-w-xs"
-          />
-        </label>
-        <label className="block p-5">
-          Email
-          <input
-            type="email"
-            name="user_email"
-            placeholder="Type here"
-            className="input input-bordered w-full max-w-xs"
-          />
-        </label>
-        <label className="block p-5">
-          Subject
-          <input
-            type="text"
-            name="subject"
-            placeholder="Type here"
-            className="input input-bordered w-full max-w-xs"
-          />
-        </label>
-        <label className="block p-5">
-          Message
-          <textarea
-            name="message"
-            className="textarea textarea-bordered"
-            placeholder="Write Message"
-          />
-        </label>
-        <input
-          className="m-3 rounded-md bg-red-400 p-3"
-          type="submit"
-          value="Send"
-        />
-      </form>
-    </>
+    <main className="m-auto mb-10 w-full p-5 md:w-[900px]">
+      <h1 className="p-5 text-center text-3xl font-bold">
+        Get In Touch With Us
+      </h1>
+      <div className="flex flex-col md:flex-row">
+        <div className="w-full md:w-1/2">
+          <Lottie options={defaultOptions} height={400} width={400} />
+        </div>
+
+        <div className="w-full md:w-1/2">
+          <form ref={form} onSubmit={sendEmail} action="" className="">
+            <div className="form__div">
+              <input
+                name="user_name"
+                type="text"
+                className="form__input"
+                placeholder=" "
+              />
+              <label className="form__label">Name</label>
+            </div>
+
+            <div className="form__div">
+              <input
+                name="user_email"
+                type="email"
+                className="form__input"
+                placeholder=" "
+              />
+              <label className="form__label">Email</label>
+            </div>
+
+            <div className="form__div">
+              <input
+                name="subject"
+                type="text"
+                className="form__input"
+                placeholder=" "
+              />
+              <label className="form__label">Subject</label>
+            </div>
+
+            <div className="form__div">
+              <textarea
+                name="message"
+                className="form__textarea"
+                placeholder="Message"
+              />
+            </div>
+
+            <input
+              type="submit"
+              className="form__button"
+              value="Send Message"
+            />
+          </form>
+        </div>
+      </div>
+    </main>
   );
 };
 
