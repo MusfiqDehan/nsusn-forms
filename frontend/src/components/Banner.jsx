46cf03b38826169fc606d9306ed236224401cb0f
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2024-02-01 19:40:41 +0600

    Update banner responsiveness and width to fix words per line

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -17,27 +17,26 @@ const Banner = () => {
         <Lottie
           className="w-full max-w-md lg:max-w-lg"
           options={ideaAnimationOptions}
-          height={400}
-          width={600}
+          height="100%"
+          width="100%"
         />
-        <div className="w-2/3 text-center lg:ml-10 lg:text-left">
+        <div className="w-1/2 text-center lg:ml-10 lg:text-left">
           <h1 className="text-3xl font-bold lg:text-6xl">
             The Next LaunchPad <br />
             is Coming!
           </h1>
-          <p className="py-6">
-            NSU Startup's Next is delighted to host The Next LaunchPad -
-            Flagship Incubation 2024. Prepare yourself for a transformative
-            journey as part of our Third Cohort! To Pave The Pathway to Your
-            Successful Venture....
+          <p className="py-6 lg:text-justify">
+            NSU Startups Next is delighted to host The Next LaunchPad - Flagship
+            Incubation 2024. Prepare yourself for a transformative journey as
+            part of our Third Cohort! To Pave The Pathway to Your Successful
+            Venture....
           </p>
           <a
             href="#apply-now"
-            className="btn bg-purple-900 px-10 text-2xl text-white hover:bg-purple-800 hover:px-11 hover:shadow-xl hover:transition "
+            className="btn bg-purple-900 px-10 text-2xl text-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-purple-800 hover:px-11 hover:shadow-xl"
           >
             Apply Now!
           </a>
-          <div></div>
         </div>
       </div>
     </div>
