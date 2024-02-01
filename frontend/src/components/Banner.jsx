ef5b3d92481d8760a5af30b14606ca7b87be19a7
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2024-02-01 21:55:29 +0600

    Try to add RocketAnimation and animation toggle

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,7 +1,11 @@
+import { useState, useEffect } from "react";
 import Lottie from "react-lottie";
 import IdeaAnimation from "../lotties/IdeaAnimation.json";
+import RocketAnimation from "../lotties/RocketAnimation.json";
 
 const Banner = () => {
+  const [showIdeaAnimation, setShowIdeaAnimation] = useState(true);
+
   const ideaAnimationOptions = {
     loop: true,
     autoplay: true,
@@ -11,15 +15,49 @@ const Banner = () => {
     },
   };
 
+  const RocketAnimationOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: RocketAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setShowIdeaAnimation((prev) => !prev);
+    }, 7000); // Change animation every 5 seconds
+
+    return () => clearInterval(timer); // Clean up on component unmount
+  }, []);
+
   return (
     <div className="hero flex min-h-screen flex-col items-center justify-center">
       <div className="hero-content flex-col lg:flex-row-reverse">
+        {/* {showIdeaAnimation ? (
+          <Lottie
+            className="w-full max-w-md lg:max-w-lg"
+            options={ideaAnimationOptions}
+            height="100%"
+            width="100%"
+          />
+        ) : (
+          <Lottie
+            className="w-full max-w-md lg:max-w-lg"
+            options={RocketAnimationOptions}
+            height="100%"
+            width="100%"
+          />
+        )} */}
+
         <Lottie
           className="w-full max-w-md lg:max-w-lg"
           options={ideaAnimationOptions}
           height="100%"
           width="100%"
         />
+
         <div className="w-1/2 text-center lg:ml-10 lg:text-left">
           <h1 className="text-3xl font-bold lg:text-6xl">
             The Next LaunchPad <br />
@@ -33,7 +71,7 @@ const Banner = () => {
           </p>
           <a
             href="#apply-now"
-            className="btn bg-purple-900 px-10 text-2xl text-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-purple-800 hover:px-11 hover:shadow-xl"
+            className="btn bg-purple-900 px-10 text-xl text-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-purple-800 hover:px-11 hover:shadow-xl lg:text-2xl"
           >
             Apply Now!
           </a>
